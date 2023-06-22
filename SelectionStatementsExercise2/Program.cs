e8b9691ab07be85701d2bf8894722340ec7ff511
eopong87 <eopong87@gmail.com>
2023-06-22 09:14:57 -0400

    subjects

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Whats your favorite school subject?");
+            var subject = Console.ReadLine();
+
+            switch(subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is my favorite also");
+                    break;
+
+                case "English":
+                    Console.WriteLine("My least fav.");
+                    break;
+                case "Science":
+                    Console.WriteLine("Nice");
+                    break;
+
+                case "History":
+                    Console.WriteLine("Thats a good ne");
+                    break;
+
+                case "Language Arts":
+                    Console.WriteLine("Great!!!!");
+                    break;
+                default:
+                    Console.WriteLine($"I was never a fanofthat class!");
+                    break;
+            }
         }
     }
 }
